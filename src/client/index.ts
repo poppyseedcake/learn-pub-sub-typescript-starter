aa90e9d8d030a0a474a911b512fca76a5debcd38
poppyseedcake <w.makowiec@gmail.com>
2026-05-02 00:11:54 +0200

    bootdev queue

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,14 +1,12 @@
 import amqp from "amqplib";
 import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
-import { declareAndBind, SimpleQueueType } from "../internal/pubsub/queue.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
-
-  const username = await clientWelcome();
-  await declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient);
+  console.log("Peril game client connected to RabbitMQ!");
 
   ["SIGINT", "SIGTERM"].forEach((signal) =>
     process.on(signal, async () => {
@@ -21,6 +19,16 @@ async function main() {
         process.exit(0);
       }
     }),
+  );
+
+  const username = await clientWelcome();
+
+  await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    `${PauseKey}.${username}`,
+    PauseKey,
+    SimpleQueueType.Transient,
   );
 }
 
